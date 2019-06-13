841448a3698e0cc0a4f98ffeb84dade5b77f91b9
vagrant <vagrant@gcloud>
2019-06-13 13:20:17 +0000

    complete homework9

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,7 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+ } 
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+ }
---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,7 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+ } 
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base" 
+}
