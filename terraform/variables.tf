fa5c4712a335522288fa9eecacb414e87ee3c79c
vagrant <vagrant@gcloud>
2019-06-13 08:58:50 +0000

    splitted terraform version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,11 @@ variable public_key_path {
 variable disk_image {
   description = "Disk image"
 }
+variable app_disk_image {
+    description = "Disk image for reddit app"
+    default     = "reddit-app-base"  
+}
+variable db_disk_image { 
+    description = "Disk image for reddit db"
+    default     = "reddit-db-base"  
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,13 @@
+resource "google_compute_firewall" "firewall_ssh" {
+    name    = "allow-ssh-default"
+ # Название сети, в которой действует правило
+    network = "default"
+ # Какой доступ разрешить
+    allow {
+      protocol = "tcp"
+      ports    = ["22"]
+    }
+ # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
+}
+
